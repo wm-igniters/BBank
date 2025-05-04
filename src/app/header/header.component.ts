7a10a752e16f9228d47c460e56cbc0fa3b3e96c0
Venkatesh Kakani <42104384-venkateswarluk5@users.noreply.replit.com>
2025-05-04 17:01:38 +0000

    Restored to '97452fcb14e2f8aa188d1a8bfb4ea05d4d316a74'
    Replit-Restored-To: 97452fcb14e2f8aa188d1a8bfb4ea05d4d316a74

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,6 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
-import { AuthService, User } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -11,23 +9,7 @@ import { AuthService, User } from '../services/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  currentUser: User | null = null;
-  lastLogin: string = '';
-
-  constructor(private authService: AuthService, private router: Router) {}
-
-  ngOnInit() {
-    this.authService.getCurrentUser().subscribe(user => {
-      this.currentUser = user;
-    });
-    this.authService.getLastLogin().subscribe(time => {
-      this.lastLogin = time;
-    });
-  }
-
-  logout() {
-    this.authService.logout();
-    this.router.navigate(['/login']);
-  }
+export class HeaderComponent {
+  @Input() userName: string = '';
+  @Input() lastLogin: string = '';
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,57 +1,15 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject, tap } from 'rxjs';
-
-export interface User {
-  id: string;
-  username: string;
-  name: string;
-  email: string;
-  role: string;
-}
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private currentUserSubject = new BehaviorSubject<User | null>(null);
-  private lastLoginTime = new BehaviorSubject<string>('');
-  
-  constructor(private http: HttpClient) {
-    const savedUser = localStorage.getItem('currentUser');
-    if (savedUser) {
-      this.currentUserSubject.next(JSON.parse(savedUser));
-      this.lastLoginTime.next(localStorage.getItem('lastLogin') || '');
-    }
-  }
+  constructor(private http: HttpClient) {}
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post('/academy-custom/login', { username, password }).pipe(
-      tap(response => {
-        const currentTime = new Date().toLocaleString();
-        localStorage.setItem('currentUser', JSON.stringify(response.user));
-        localStorage.setItem('token', response.token);
-        localStorage.setItem('lastLogin', currentTime);
-        this.currentUserSubject.next(response.user);
-        this.lastLoginTime.next(currentTime);
-      })
-    );
-  }
-
-  logout() {
-    localStorage.removeItem('currentUser');
-    localStorage.removeItem('token');
-    localStorage.removeItem('lastLogin');
-    this.currentUserSubject.next(null);
-    this.lastLoginTime.next('');
-  }
-
-  getCurrentUser() {
-    return this.currentUserSubject.asObservable();
-  }
-
-  getLastLogin() {
-    return this.lastLoginTime.asObservable();
+    return this.http.post('/academy-custom/login', { username, password });
   }
 }
